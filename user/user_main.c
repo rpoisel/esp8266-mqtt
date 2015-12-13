571614274c92aa2d099158c964559b0b902662e0
Rainer Poisel <rainer.poisel@gmail.com>
2015-12-13 18:04:12 +0000

    counter and GPIO access

--- user/user_main.c
+++ user/user_main.c
@@ -39,6 +39,21 @@
 #include "mem.h"
 
 MQTT_Client mqttClient;
+static ETSTimer publishTimer;
+static BOOL mqttConnected = FALSE;
+static uint32_t count = 0;
+static ETSTimer counterIncrementTimer;
+
+#define PUBLISH_DATA_LEN 128
+#define MQTT_TOPIC_LED "/mqtt/led/green"
+#define MQTT_TOPIC_COUNT_0 "/mqtt/count/0"
+#define LED_ON "on"
+#define LED_OFF "off"
+#define INTERVAL_PUBLISH 1000 /* milliseconds */
+#define INTERVAL_INCREMENT_COUNT 500 /* milliseconds */
+
+static void ICACHE_FLASH_ATTR publish_counter_value(void *args);
+static void ICACHE_FLASH_ATTR increment_counter_value(void *args);
 
 void wifiConnectCb(uint8_t status)
 {
@@ -56,20 +71,22 @@ void mqttConnectedCb(uint32_t *args)
 {
   MQTT_Client* client = (MQTT_Client*) args;
   INFO("MQTT: Connected\r\n");
-  MQTT_Subscribe(client, "/mqtt/topic/0", 0);
-  MQTT_Subscribe(client, "/mqtt/topic/1", 1);
-  MQTT_Subscribe(client, "/mqtt/topic/2", 2);
 
-  MQTT_Publish(client, "/mqtt/topic/0", "hello0", 6, 0, 0);
-  MQTT_Publish(client, "/mqtt/topic/1", "hello1", 6, 1, 0);
-  MQTT_Publish(client, "/mqtt/topic/2", "hello2", 6, 2, 0);
+  MQTT_Subscribe(client, MQTT_TOPIC_LED, 0);
 
+  os_timer_disarm(&publishTimer);
+  os_timer_setfn(&publishTimer, (os_timer_func_t *)publish_counter_value, client);
+  os_timer_arm(&publishTimer, INTERVAL_PUBLISH, 0);
+
+  mqttConnected = TRUE;
 }
 
 void mqttDisconnectedCb(uint32_t *args)
 {
   MQTT_Client* client = (MQTT_Client*) args;
   INFO("MQTT: Disconnected\r\n");
+
+  mqttConnected = FALSE;
 }
 
 void mqttPublishedCb(uint32_t *args)
@@ -91,9 +108,56 @@ void mqttDataCb(uint32_t *args, const char* topic, uint32_t topic_len, const cha
   os_memcpy(dataBuf, data, data_len);
   dataBuf[data_len] = 0;
 
-  INFO("Receive topic: %s, data: %s \r\n", topicBuf, dataBuf);
+  INFO("Receive topic: '%s', data: '%s'\r\n", topicBuf, dataBuf);
+
+  if (os_strcmp(topicBuf, MQTT_TOPIC_LED) == 0)
+  {
+    if (os_strcmp(dataBuf, LED_ON) == 0)
+    {
+      gpio_output_set(BIT13, 0, BIT13, 0);
+      INFO("LED on\r\n");
+    }
+    else if (os_strcmp(dataBuf, LED_OFF) == 0)
+    {
+      gpio_output_set(0, BIT13, BIT13, 0);
+      INFO("LED off\r\n");
+    }
+    else
+    {
+      INFO("INVALID state for LED\r\n");
+    }
+  }
+
   os_free(topicBuf);
   os_free(dataBuf);
+}
+
+static void ICACHE_FLASH_ATTR publish_counter_value(void *args)
+{
+  MQTT_Client* client = (MQTT_Client*) args;
+
+  os_timer_disarm(&publishTimer);
+
+  char *dataBuf = (char*) os_zalloc(PUBLISH_DATA_LEN);
+
+  if (!mqttConnected)
+  {
+    return;
+  }
+
+  os_sprintf(dataBuf, "%d", count);
+
+  INFO("Publishing: %s\r\n", dataBuf);
+  MQTT_Publish(client, "/mqtt/count/0", dataBuf, os_strlen(dataBuf), 0, 0);
+  os_free(dataBuf);
+
+  os_timer_setfn(&publishTimer, (os_timer_func_t *)publish_counter_value, client);
+  os_timer_arm(&publishTimer, INTERVAL_PUBLISH, 0);
+}
+
+static void ICACHE_FLASH_ATTR increment_counter_value(void *args)
+{
+  count++;
 }
 
 void user_init(void)
@@ -103,11 +167,12 @@ void user_init(void)
 
   CFG_Load();
 
+  PIN_FUNC_SELECT(PERIPHS_IO_MUX_MTCK_U, FUNC_GPIO13);
+  gpio_output_set(0, BIT13, BIT13, 0);
+
   MQTT_InitConnection(&mqttClient, sysCfg.mqtt_host, sysCfg.mqtt_port, sysCfg.security);
-  //MQTT_InitConnection(&mqttClient, "192.168.11.122", 1880, 0);
 
   MQTT_InitClient(&mqttClient, sysCfg.device_id, sysCfg.mqtt_user, sysCfg.mqtt_pass, sysCfg.mqtt_keepalive, 1);
-  //MQTT_InitClient(&mqttClient, "client_id", "user", "pass", 120, 1);
 
   MQTT_InitLWT(&mqttClient, "/lwt", "offline", 0, 0);
   MQTT_OnConnected(&mqttClient, mqttConnectedCb);
@@ -117,6 +182,10 @@ void user_init(void)
 
   WIFI_Connect(sysCfg.sta_ssid, sysCfg.sta_pwd, wifiConnectCb);
   INFO("SSID: %s, Password: %s\r\n", sysCfg.sta_ssid, sysCfg.sta_pwd);
+
+  os_timer_disarm(&counterIncrementTimer);
+  os_timer_setfn(&counterIncrementTimer, (os_timer_func_t *)increment_counter_value, NULL);
+  os_timer_arm(&counterIncrementTimer, INTERVAL_INCREMENT_COUNT, 1);
 
   INFO("\r\nSystem started ...\r\n");
 }
